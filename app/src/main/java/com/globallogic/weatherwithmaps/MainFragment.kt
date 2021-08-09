e148b73d8433bdb42e83a83ad72b7234857174b0
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-08-09 09:28:44 +0200

    Setup maps fragment

---
+++ app/src/main/java/com/globallogic/weatherwithmaps/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.globallogic.weatherwithmaps
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.globallogic.weatherwithmaps.databinding.FragmentMainBinding
+import com.tomtom.online.sdk.map.*
+
+class MainFragment : Fragment(), OnMapReadyCallback {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
+        mapFragment.getAsyncMap(this)
+    }
+
+    override fun onMapReady(tomtomMap: TomtomMap) {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
