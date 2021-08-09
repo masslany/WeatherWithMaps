5e36f46e409fb7d8a0f40389abc14b4aef121bda
Patryk Maslanka <patryk.maslanka@globallogic.com>
2021-08-09 12:35:21 +0200

    Add dragable marker with location updates

--- app/src/main/java/com/globallogic/weatherwithmaps/presentation/weather/WeatherFragment.kt
+++ app/src/main/java/com/globallogic/weatherwithmaps/presentation/weather/WeatherFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.globallogic.weatherwithmaps.R
+import com.globallogic.weatherwithmaps.data.remote.response.weather.WeatherResponse
 import com.globallogic.weatherwithmaps.databinding.FragmentWeatherBinding
+import com.globallogic.weatherwithmaps.domain.model.Location
 import com.globallogic.weatherwithmaps.presentation.util.State
-import com.tomtom.online.sdk.map.MapFragment
-import com.tomtom.online.sdk.map.OnMapReadyCallback
-import com.tomtom.online.sdk.map.TomtomMap
+import com.tomtom.online.sdk.common.location.LatLng
+import com.tomtom.online.sdk.map.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -38,40 +40,61 @@ class WeatherFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getAsyncMap(this)
 
         viewModel.weather.observe(viewLifecycleOwner) { state ->
-            println(state)
-            when (state) {
-                State.Loading -> {
-                    binding.progressBar.visibility = View.VISIBLE
-                }
-
-                is State.Success -> {
-                    binding.progressBar.visibility = View.GONE
-
-                }
-
-                is State.Error -> {
-                    binding.progressBar.visibility = View.GONE
-                }
-            }
+            handleWeatherState(state)
         }
         viewModel.locationName.observe(viewLifecycleOwner) { state ->
-            when (state) {
-                State.Loading -> {
-                }
-                is State.Success -> {
-                    binding.tvCityName.text = state.data
-                }
-                is State.Error -> {
-                    binding.tvCityName.text = "Error"
-                }
-            }
+            handleLocationNameState(state)
         }
-
 
     }
 
-    override fun onMapReady(tomtomMap: TomtomMap) {
+    private fun handleLocationNameState(state: State<String>) {
+        when (state) {
+            State.Loading -> {
+            }
+            is State.Success -> {
+                binding.tvCityName.text = state.data
+            }
+            is State.Error -> {
+                binding.tvCityName.text = "Error"
+            }
+        }
+    }
 
+    private fun handleWeatherState(state: State<WeatherResponse>) {
+        when (state) {
+            State.Loading -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+
+            is State.Success -> {
+                binding.progressBar.visibility = View.GONE
+
+            }
+
+            is State.Error -> {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+    }
+
+    private var onMarkerDragListener: TomtomMapCallback.OnMarkerDragListener =
+        object : TomtomMapCallback.OnMarkerDragListener {
+            override fun onStartDragging(marker: Marker) {}
+
+            override fun onStopDragging(marker: Marker) {
+                val location = Location(marker.position.latitude, marker.position.longitude)
+                viewModel.onLocationChanged(location)
+            }
+
+            override fun onDragging(marker: Marker) {}
+        }
+
+    override fun onMapReady(tomtomMap: TomtomMap) {
+        val markerBuilder = MarkerBuilder(LatLng(53.4138608, 14.5557252))
+            .draggable(true)
+        tomtomMap.addMarker(markerBuilder)
+        tomtomMap.addOnMarkerDragListener(onMarkerDragListener)
     }
 
     override fun onDestroy() {
